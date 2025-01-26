69c2c7e10b30a0a99429953619876ebb0d626ce7
lennartzellmer <2241624+lennartzellmer@users.noreply.github.com>
2025-01-26 09:55:38 +0100

    WIP

--- app/machines/fetchPaginated/__tests__/fetchPaginated.spec.ts
+++ app/machines/fetchPaginated/__tests__/fetchPaginated.spec.ts
@@ -3,19 +3,25 @@ import { createActor } from 'xstate'
 import { createFetchPaginatedMachine } from '../fetchPaginated.machine'
 import { filterSearchMachine } from '../../../machines/filters/filterSearch.machine'
 
-describe('filterMachin', () => {
+describe('fetchPaginatedMachine', () => {
   it('stores ininally on creation', () => new Promise((done) => {
+    global.window = Object.create(window)
+    const url = new URL('https://www.example.com')
+    url.searchParams.set('searchTerm', 'test')
+    Object.defineProperty(window, 'location', {
+      value: url
+    })
+
     const actor = createActor(createFetchPaginatedMachine({
       filterMachines: [filterSearchMachine]
     }), {
       inspect(inspectionEvent) {
         if (inspectionEvent.type === '@xstate.event') {
-          // console.log('inspectionEvent', inspectionEvent)
+          console.log('inspectionEvent', inspectionEvent.event)
         }
       }
     })
     actor.start()
-    console.log('actor', actor.getSnapshot().context)
     done('')
   }))
 })
