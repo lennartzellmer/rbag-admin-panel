765c2eec6ff8aa7ea2fe1de208bc197704c2ec45
lennartzellmer <2241624+lennartzellmer@users.noreply.github.com>
2025-01-27 23:52:13 +0100

    WIP

--- app/machines/genericFilter/__tests__/genericFilter.spec.ts
+++ app/machines/genericFilter/__tests__/genericFilter.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { createActor, createEmptyActor } from 'xstate'
+import { createActor, createEmptyActor, createMachine } from 'xstate'
 import { createFilterMachine } from '../genericFilter.machine'
 
 const URL_KEY = 'foo'
@@ -46,46 +46,37 @@ describe('filterMachineCommunicatesToParent', () => {
   it('sends FILTER_UPDATED event to parent when filter is updated', () => new Promise((done) => {
     const filterValue = new URLSearchParams()
     filterValue.set('foo', 'secondEvent')
-    const parent = createEmptyActor()
-    const actor = createActor(filterTestMachine.machine, {
-      parent,
-      // Use inspect to check if the FILTER_UPDATED event is received on the parent
-      inspect: (inspEvent) => {
+    let receivedEvent = undefined
+    const parent = createActor(createMachine({ id: 'parent' }), {
+      inspect(inspEvent) {
         // Only respond to actual XState events
-        console.log(inspEvent)
-        if (inspEvent.type === '@xstate.event') {
-          // Only respond to FILTER_UPDATED events
-          if (inspEvent.event.type === 'FILTER_UPDATED') {
-            // Check if it has the specific data we want
-            if (inspEvent.event.filter?.filterValue.get('foo') === 'secondEvent') {
-              expect(inspEvent.event).toEqual({ type: 'FILTER_UPDATED', filter: { filterParamsKey: FILTER_PARAMS_KEY, filterValue: filterValue } })
-              done('')
-            }
+        if (inspEvent.type === '@xstate.event' && inspEvent.event.type === 'FILTER_UPDATED') {
+          // Check if it has the specific data we want
+          if (inspEvent.event.filter?.filterValue.get('foo') === 'secondEvent') {
+            receivedEvent = inspEvent.event
           }
         }
       }
     })
-    actor.start()
+    const actor = createActor(filterTestMachine.machine, { parent }).start()
     actor.send({ type: 'SET_FILTER', payload: { filterParamsKey: FILTER_PARAMS_KEY, filterValue: filterValue } })
+    expect(receivedEvent).toEqual({ type: 'FILTER_UPDATED', filter: { filterParamsKey: FILTER_PARAMS_KEY, filterValue: filterValue } })
+    done('')
   }))
 
   it('sends FILTER_RESET event to parent when filter is removed', () => new Promise((done) => {
-    const parent = createEmptyActor()
-    const actor = createActor(filterTestMachine.machine, {
-      parent,
-      // Use inspect to check if the FILTER_UPDATED event is received on the parent
-      inspect: (inspEvent) => {
-        // Only respond to actual XState events
-        if (inspEvent.type === '@xstate.event') {
-          // Only respond to FILTER_UPDATED events
-          if (inspEvent.event.type === 'FILTER_RESET') {
-            expect(inspEvent.event).toEqual({ type: 'FILTER_RESET', filterParamsKey: FILTER_PARAMS_KEY })
-            done('')
-          }
+    let receivedEvent = undefined
+    const parent = createActor(createMachine({ id: 'parent' }), {
+      inspect(inspectionEvent) {
+        if (inspectionEvent.type === '@xstate.event' && inspectionEvent.event.type === 'FILTER_RESET') {
+          receivedEvent = inspectionEvent.event
         }
       }
     })
-    actor.start()
+    const actor = createActor(filterTestMachine.machine, { parent }).start()
     actor.send({ type: 'SET_FILTER', payload: null })
+
+    expect(receivedEvent).toEqual({ type: 'FILTER_RESET', filterParamsKey: FILTER_PARAMS_KEY })
+    done('')
   }))
 })
