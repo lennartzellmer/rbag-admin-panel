9a25c3a148eba021643fa3f89824ecd149f4aa2e
lennartzellmer <2241624+lennartzellmer@users.noreply.github.com>
2025-09-09 16:46:17 +0200

    WIP

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,5 +1,18 @@
 export default defineEventHandler(async (event) => {
   if (event.path.startsWith('/api/admin')) {
-    await requireUserSession(event)
+    const { user } = await requireUserSession(event) as unknown as { user: User }
+    event.context.user = user
   }
 })
+
+interface User {
+  email: string
+  name: string
+}
+
+// Extend the H3EventContext interface to include the eventStore property
+declare module 'h3' {
+  interface H3EventContext {
+    user?: User
+  }
+}
