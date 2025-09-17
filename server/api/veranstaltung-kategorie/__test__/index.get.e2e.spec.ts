e2fa1633047ea0e82419b2890cec724fc382062d
lennartzellmer <2241624+lennartzellmer@users.noreply.github.com>
2025-09-17 13:15:33 +0200

    WIP

---
+++ server/api/veranstaltung-kategorie/__test__/index.get.e2e.spec.ts
@@ -0,0 +1,70 @@
+import { beforeAll, describe, expect, test } from 'vitest'
+import { setup, $fetch } from '@nuxt/test-utils/e2e'
+import type { PaginationResponseSchema } from '~~/validation/paginationQuerySchema'
+
+describe('VeranstaltungsKategorie GET - E2E Test', async () => {
+  await setup({})
+
+  beforeAll(async () => {
+    await $fetch('/api/veranstaltung-kategorie/create', {
+      method: 'POST',
+      body: {
+        name: 'Kategorie A'
+      }
+    })
+
+    await $fetch('/api/veranstaltung-kategorie/create', {
+      method: 'POST',
+      body: {
+        name: 'Kategorie B'
+      }
+    })
+  })
+
+  test('should return paginated veranstaltungsKategorien with metadata', async () => {
+    const response = await $fetch<PaginationResponseSchema>('/api/veranstaltung-kategorie', {
+      query: {
+        limit: '10',
+        offset: '0'
+      }
+    })
+
+    expect(response).toMatchSnapshot({
+      data: expect.arrayContaining([
+        expect.objectContaining({
+          name: expect.any(String)
+        })
+      ]),
+      meta: {
+        total: expect.any(Number),
+        offset: 0,
+        limit: 10
+      }
+    })
+
+    console.log('djkashkjdhajkshdkakjshdjkahskhwqwe', response)
+
+    expect(response).toBeDefined()
+    expect(response.data).toEqual(expect.arrayContaining([
+      expect.objectContaining({
+        name: 'Kategorie A'
+      }),
+      expect.objectContaining({
+        name: 'Kategorie B'
+      })
+    ]))
+
+    expect(response.meta.total).toBeGreaterThanOrEqual(2)
+  })
+
+  // test('should return 400 for invalid pagination query params', async () => {
+  //   await expect(() => $fetch('/api/veranstaltung-kategorie', {
+  //     query: {
+  //       limit: 'abc',
+  //       offset: '0'
+  //     }
+  //   })).rejects.toThrowError(expect.objectContaining({
+  //     statusCode: 400
+  //   }))
+  // })
+})
--- validation/paginationQuerySchema.ts
+++ validation/paginationQuerySchema.ts
@@ -4,3 +4,15 @@ export const paginationQuerySchema = z.object({
   limit: z.string().transform(Number).default('10'),
   offset: z.string().transform(Number).default('0')
 })
+
+export const paginationResponseSchema = z.object({
+  data: z.array(z.any()),
+  meta: z.object({
+    total: z.number().min(0),
+    offset: z.number().min(0),
+    limit: z.number().min(1)
+  })
+})
+
+export type PaginationQuerySchema = z.infer<typeof paginationQuerySchema>
+export type PaginationResponseSchema = z.infer<typeof paginationResponseSchema>
