6417f803d64117e3eda65d664718ec5a52d1048f
lennartzellmer <2241624+lennartzellmer@users.noreply.github.com>
2025-09-17 14:33:32 +0200

    WIP

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,23 @@
 // @ts-check
+import unusedImports from 'eslint-plugin-unused-imports'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  name: 'project/unused-imports',
+  plugins: {
+    'unused-imports': unusedImports
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ]
+  }
+})
