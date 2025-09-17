d45b47c064d3e562a16f9c874caefbcede8f62b2
lennartzellmer <2241624+lennartzellmer@users.noreply.github.com>
2025-09-17 11:07:36 +0200

    WIP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2024-11-01',
+  nitro: {
+    ignore: ['**/*.spec.ts', '**/__test__/**']
+  },
   eslint: {
     config: {
       stylistic: {
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,10 +5,18 @@ export default defineConfig({
   test: {
     silent: false,
     projects: [
+      {
+        test: {
+          name: 'unit',
+          include: ['server/**/*.{test,spec}.ts'],
+          exclude: ['**/*.e2e.spec.ts'],
+          environment: 'node'
+        }
+      },
       await defineVitestProject({
         test: {
           name: 'e2e',
-          include: ['test/e2e/**/*.e2e.spec.ts'],
+          include: ['server/**/*.e2e.spec.ts'],
           environment: 'nuxt'
         }
       }),
