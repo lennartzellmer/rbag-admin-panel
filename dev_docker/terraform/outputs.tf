a0a5c80db9f79b41af4e863b1c0ab3d672f2485c
lennartzellmer <2241624+lennartzellmer@users.noreply.github.com>
2025-12-19 10:11:24 +0100

    WIP

--- dev_docker/terraform/outputs.tf
+++ dev_docker/terraform/outputs.tf
@@ -2,3 +2,11 @@ output "zitadel_backend_pat" {
   value     = module.machine_user_pat.pat
   sensitive = true
 }
+
+output "zitadel_org_id" {
+  value = module.org.org_id
+}
+
+output "zitadel_project_id" {
+  value = module.project_oidc.project_id
+}
